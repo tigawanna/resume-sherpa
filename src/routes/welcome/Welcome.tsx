c9b6eea3ee35ed4a8bc4c765e012f1348e8edc77
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2023-10-26 19:59:52 +0300

    added landidng page

--- src/routes/welcome/Welcome.tsx
+++ src/routes/welcome/Welcome.tsx
@@ -8,26 +8,7 @@ interface WelcomePageProps {}
 export function WelcomeSection({}: WelcomePageProps) {
   return (
     <div className="w-full h-full min-h-screen bg-base-300 text-base-content">
-      {/* <div className="hero-content text-center">
-        <div className="min-h-[200px] flex flex-col justify-evenly items-center gap-5">
-          <h1 className="text-6xl font-bold">Welcome To Sherpa</h1>
-          <p className="text-2xl text-base-content/80 ">
-            Your smart job application assistant
-          </p>
-          <Features />
-
-          <div className="flex  gap-10">
-            <Link href="/dashboard" className="btn btn-primary">
-              Get Started
-            </Link>
-            <Link href="/auth" className="btn btn-primary">
-              Or login to continue
-            </Link>
-          </div>
-        </div>
-      </div> */}
-      {/* <HeroSection/> */}
-      <IntroSection/>
+    <IntroSection/>
       <DescriptionBulletPoints/>
       <LastStep/>
     </div>
--- src/routes/welcome/hero-sections/IntroSection.tsx
+++ src/routes/welcome/hero-sections/IntroSection.tsx
@@ -1,35 +1,33 @@
 import { Image } from "@unpic/react";
 import { Link } from "rakkasjs";
 
-interface IntroSectionProps {
+interface IntroSectionProps {}
 
-}
-
-export function IntroSection({}:IntroSectionProps){
-return (
-<section className="w-full h-full flex min-h-screen justify-start items-start">
-		<div className="w-full flex flex-col  p-6 mx-auto  lg:flex-row lg:justify-between">
-			<div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-				<h1 className="text-6xl font-bold sm:text-6xl">Sherpa</h1>
-				<p className="mt-6 mb-8 text-lg font-normal sm:mb-12">Scaffold out a job specific resume and cover letter ,
-                <br />
-                Easily create tailored resumes and cover letters for every job application
-                    </p>
-				<div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-				<Link href="/dashboard" className="btn btn-primary">
-                    Get Started
-                </Link>
-            <Link href="/auth" className="btn btn-primary">
-              Or login to continue
+export function IntroSection({}: IntroSectionProps) {
+  return (
+    <section className="w-full h-full flex min-h-screen justify-start items-start">
+      <div className="w-full flex flex-col p-5 sm:p-6 sm:mx-auto  lg:flex-row lg:justify-between gap-5">
+        <div className="flex min-h-[350px] flex-col justify-center sm:p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+          <h1 className="text-7xl font-bold sm:text-8xl">Sherpa</h1>
+          <p className="mt-6 mb-8  font-normal sm:text-lg sm:mb-12">
+            Scaffold out a job specific resume and cover letter, Easily create
+            tailored resumes and cover letters for every job application
+          </p>
+          <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+            <Link href="/dashboard" className="btn btn-primary">
+              Get Started
             </Link>
-				</div>
-			</div>
-			<div className="w-full lg:w-[40%] h-full min-h-screen flex items-center justify-center ">
-				<Image src="svg/Business_SVG.svg" alt="hero business page" layout="fullWidth" 
-                className="object-contain w-full max-h-[70%]" />
-			</div>
-		</div>
-	</section>
-
-);
+          </div>
+        </div>
+        <div className="w-full lg:w-[40%] h-full sm:min-h-screen flex items-center justify-center ">
+          <Image
+            src="svg/Business_SVG.svg"
+            alt="hero business page"
+            layout="fullWidth"
+            className="object-contain w-full max-h-[70%]"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
